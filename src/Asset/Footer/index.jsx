334df3245622de0d54e0d3f99fe3b468bca125c7
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-05-14 04:16:12 +0530

    Add: Footer

---
+++ src/Asset/Footer/index.jsx
@@ -0,0 +1,8 @@
+import Twitter from "./Twitter_Footer.svg";
+import Facebook from "./Facebook_Footer.svg";
+import Discord from "./Discord_Footer.svg";
+import Instagram from "./Instagram_Footer.svg";
+import Twitch from "./Twitch_Footer.svg";
+import Linkedin from "./Linkedin_Footer.svg";
+
+export { Twitter, Facebook, Discord, Instagram, Twitch, Linkedin };
--- src/Component/Layout/Footer/index.jsx
+++ src/Component/Layout/Footer/index.jsx
@@ -1,9 +1,72 @@
 import React from "react";
-
+import NavLogo from "../../../Asset/Navbar/NavLogo.png";
 import "./Footer.css";
+import { Link } from "react-router-dom";
+import {
+  Discord,
+  Twitch,
+  Twitter,
+  Linkedin,
+  Facebook,
+  Instagram,
+} from "../../../Asset/Footer";
 
 function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="footer">
+      <div className="footer_container">
+        {/* HEADER */}
+        <div className="footer_header">
+          <img src={NavLogo} alt="footer_logo" />
+        </div>
+        {/* BODY */}
+        <div className="footer_body">
+          <div className="footer_primary">
+            <div className="footer_primary_row">
+              <Link className="footer_text_link">Home</Link>
+              <Link className="footer_text_link">Store</Link>
+              <Link className="footer_text_link">Cart</Link>
+              <Link className="footer_text_link">Wish List</Link>
+              <Link className="footer_text_link">About</Link>
+              <Link className="footer_text_link">Contact</Link>
+              <Link className="footer_text_link">Policy</Link>
+              <Link className="footer_text_link">Click</Link>
+            </div>
+          </div>
+          <div className="footer_secondary">
+            <h3 className="footer_secondary_header">Connect With Me:</h3>
+            <div className="footer_social">
+              <Link className="footer_img_link">
+                <img src={Twitch} alt="social_logo" />
+              </Link>
+              <Link className="footer_img_link">
+                <img src={Twitter} alt="social_logo" />
+              </Link>
+              <Link className="footer_img_link">
+                <img src={Linkedin} alt="social_logo" />
+              </Link>
+              <Link className="footer_img_link">
+                <img src={Instagram} alt="social_logo" />
+              </Link>
+              <Link className="footer_img_link">
+                <img src={Discord} alt="social_logo" />
+              </Link>
+              <Link className="footer_img_link">
+                <img src={Facebook} alt="social_logo" />
+              </Link>
+            </div>
+          </div>
+        </div>
+        {/* FOOTER */}
+
+        <p className="footer_footer">
+          © 2023 Valve Corporation. All rights reserved. All trademarks are
+          property of their respective owners in the US and other countries. VAT
+          included in all prices where applicable.
+        </p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
