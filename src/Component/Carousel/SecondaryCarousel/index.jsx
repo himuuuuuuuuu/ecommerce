46b92488ffe823f1cd570a2812fc0a40200a1194
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-05-15 04:38:36 +0530

    Update: Carousel Second Data

--- src/Component/Carousel/SecondaryCarousel/index.jsx
+++ src/Component/Carousel/SecondaryCarousel/index.jsx
@@ -10,62 +10,62 @@ function SecondaryCarousel() {
       id: 0,
       title: "Action",
       thumbnail:
-        "https://media-rockstargames-com.akamaized.net/rockstargames-newsite/img/global/games/fob/640/reddeadredemption2.jpg",
+        "https://media.contentapi.ea.com/content/dam/gin/images/2019/01/apex-legends-keyart.jpg.adapt.crop1x1.767p.jpg",
     },
     {
       id: 1,
       title: "Anime",
       thumbnail:
-        "https://media-rockstargames-com.akamaized.net/rockstargames-newsite/img/global/games/fob/640/grandtheftauto3.jpg",
+        "https://media.contentapi.ea.com/content/dam/gin/images/2022/03/lost-castle-game-art-16x9.jpg.adapt.crop1x1.767p.jpg",
     },
     {
       id: 2,
       title: "Racing",
       thumbnail:
-        "https://media-rockstargames-com.akamaized.net/rockstargames-newsite/img/global/games/fob/640/vicecity.jpg",
+        "https://media.contentapi.ea.com/content/dam/gin/images/2017/01/nfs-rivals-key-art.jpg.adapt.crop1x1.767p.jpg",
     },
     {
       id: 3,
       title: "Horror",
       thumbnail:
-        "https://media-rockstargames-com.akamaized.net/rockstargames-newsite/img/global/games/fob/640/reddeadredemption.jpg",
+        "https://media.contentapi.ea.com/content/dam/gin/images/2019/06/sos-section-keyart-xl.jpg.adapt.crop1x1.767p.jpg",
     },
 
     {
       id: 4,
       title: "Sports",
       thumbnail:
-        "https://media-rockstargames-com.akamaized.net/rockstargames-newsite/img/global/games/fob/640/maxpayne.jpg",
+        "https://media.contentapi.ea.com/content/dam/gin/images/2022/07/eas-fifa22-gen5-ue-keyart-horz-f23logo.jpg.adapt.crop1x1.767p.jpg",
     },
     {
       id: 5,
       title: "Adventure",
       thumbnail:
-        "https://media-rockstargames-com.akamaized.net/rockstargames-newsite/img/global/games/fob/640/maxpayne2.jpg",
+        "https://media.contentapi.ea.com/content/dam/gin/images/2022/06/star-wars-jedi-survivor-keyart.jpg.adapt.crop1x1.767p.jpg",
     },
     {
       id: 6,
       title: "Strategy",
       thumbnail:
-        "https://media-rockstargames-com.akamaized.net/rockstargames-newsite/img/global/games/fob/640/maxpayne3.jpg",
+        "https://media.contentapi.ea.com/content/dam/gin/images/2017/01/star-wars-galaxy-of-heroes-key-art.jpg.adapt.crop1x1.767p.jpg",
     },
     {
       id: 7,
       title: "Open World",
       thumbnail:
-        "https://media-rockstargames-com.akamaized.net/rockstargames-newsite/img/global/games/fob/640/bully.jpg",
+        "https://media.contentapi.ea.com/content/dam/gin/images/2017/01/the-sims-3-pets-keyart.jpg.adapt.crop1x1.767p.jpg",
     },
     {
       id: 8,
       title: "survival",
       thumbnail:
-        "https://media-rockstargames-com.akamaized.net/rockstargames-newsite/img/global/games/fob/640/thewarriors.jpg",
+        "https://media.contentapi.ea.com/content/dam/gin/images/2020/06/rocket-arena-keyart-16x9.jpg.adapt.crop1x1.767p.jpg",
     },
     {
       id: 9,
       title: "Simulation",
       thumbnail:
-        "https://media-rockstargames-com.akamaized.net/rockstargames-newsite/img/global/games/fob/640/manhunt2.jpg",
+        "https://media.contentapi.ea.com/content/dam/gin/images/2017/01/the-sims-4-keyart.jpg.adapt.crop1x1.767p.jpg",
     },
   ];
 
