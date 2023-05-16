4f1237b0acd953cc1a5efc1bab6c7df125d5cdbd
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-05-16 23:08:58 +0530

    ADD: Card

--- src/Component/Card/PrimaryCard/index.jsx
+++ src/Component/Card/PrimaryCard/index.jsx
@@ -1,13 +1,74 @@
 import React from "react";
 import "./PrimaryCard.css";
+import ActionButton from "../../Action/ActionButton";
 
-function PrimaryCard() {
+import { ShoppingCart, Favorite } from "@mui/icons-material";
+
+function PrimaryCard(props) {
+  const {
+    id,
+    title,
+    rating,
+    price,
+    genre,
+    developer,
+    publisher,
+    release,
+    downloads,
+    esrb,
+    status,
+    age,
+    requirement,
+    recommended,
+    thumbnail,
+    logo,
+    description,
+  } = props;
+
+  const starList = ["⭐", "⭐⭐", "⭐⭐⭐", "⭐⭐⭐⭐", "⭐⭐⭐⭐⭐"];
+
   return (
     <article className="primary_card">
-      <img
-        className="primary_card_img"
-        src="https://media-rockstargames-com.akamaized.net/rockstargames-newsite/img/global/games/fob/640/reddeadredemption2.jpg"
-      />
+      <img className="primary_card_img" src={thumbnail} alt={title} />
+      <div className="primary_card_content">
+        <div className="primary_card_actions">
+          <ActionButton className="primary_card_action">
+            <ShoppingCart />
+          </ActionButton>
+          <ActionButton className="primary_card_action">
+            <Favorite />
+          </ActionButton>
+        </div>
+        <div className="primary_card_text">
+          <h3 className="primary_card_title">{title}</h3>
+          <div className="primary_card_tags">
+            <p className="primary_card_tag">
+              <strong>Price:</strong>
+              <span>{price}</span>
+            </p>
+            <p className="primary_card_tag">
+              <strong>ESRB:</strong>
+              <span>{esrb}</span>
+            </p>
+            <p className="primary_card_tag">
+              <strong>Genre:</strong>
+              <span>{genre}</span>
+            </p>
+            <p className="primary_card_tag">
+              <strong>Age:</strong>
+              <span>{age}</span>
+            </p>
+            <p className="primary_card_tag">
+              <strong>Rating:</strong>
+              <span>
+                {starList.find((currentStar, index) => {
+                  return index + 1 == rating;
+                })}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
     </article>
   );
 }
--- src/Pages/ProductList/index.jsx
+++ src/Pages/ProductList/index.jsx
@@ -4,6 +4,7 @@ import "./ProductList.css";
 import PageContainer from "../../Component/Layout/PageContainer";
 import SectionContainer from "../../Component/Layout/SectionContainer";
 import Filter from "../../Component/Filter";
+import PrimaryCard from "../../Component/Card/PrimaryCard";
 
 function ProductList() {
   const gameList = [
@@ -22,16 +23,39 @@ function ProductList() {
     // },
     {
       id: "0",
-      title: "",
-      rating: "",
-      price: "",
-      genre: "",
+      title: "Grand Theft Auto V",
+      rating: "5",
+      price: "₹2,321",
+      genre: "Adventure",
+      developer: "Rockstar Games",
+      publisher: "Rockstar Games",
+      release: "05/14/20",
+      downloads: "175",
+      esrb: "R",
+      status: "online",
+      age: "18+",
       requirement: {
-        minimum: { memory: "", storage: "", cpu: "", gpu: "", os: "" },
-        recommended: { memory: "", storage: "", cpu: "", gpu: "", os: "" },
+        minimum: {
+          memory: "4",
+          storage: "90",
+          cpu: "Intel Core 2 Quad CPU Q6600 @ 2.40GHz (4 CPUs)",
+          gpu: "NVIDIA 9800 GT 1GB",
+          os: "Windows 10 64 Bit",
+        },
+        recommended: {
+          memory: "8",
+          storage: "90",
+          cpu: "Intel Core i5 3470 @ 3.2GHZ (4 CPUs)",
+          gpu: "NVIDIA GTX 660 2GB",
+          os: "Windows 10 64 Bit",
+        },
       },
+      thumbnail:
+        "https://media-rockstargames-com.akamaized.net/rockstargames-newsite/img/global/games/fob/640/GTAOnline.jpg",
       imageList: [],
-      thumbnail: "",
+      logo: "https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fgrand-theft-auto-v%2Fhome%2FGTAV_EGS_Artwork_400x400_GameLogo-1000x1000-f6c47a98454049a5e63959f7b6f898c4fc22829c.png?h=270&quality=medium&resize=1&w=480",
+      description:
+        "The Grand Theft Auto V: Premium Edition includes the complete GTAV story, Grand Theft Auto Online and all existing gameplay upgrades and content. You’ll also get the Criminal Enterprise Starter Pack, the fastest way to jumpstart your criminal empire in GTA Online.",
     },
   ];
   return (
@@ -40,7 +64,11 @@ function ProductList() {
         <Filter />
         <div className="product_list_content">
           <h3 className="product_list_content_head">Products</h3>
-          <div className="product_list_content_body"></div>
+          <div className="product_list_content_body">
+            {gameList.map((currentGame) => {
+              return <PrimaryCard {...currentGame} key={currentGame.id} />;
+            })}
+          </div>
         </div>
       </SectionContainer>
     </PageContainer>
