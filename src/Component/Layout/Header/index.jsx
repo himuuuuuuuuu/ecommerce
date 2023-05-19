91e328163d8d84449f527a77a4712f55614bf3dc
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-05-19 14:47:26 +0530

    STYLE: Product Detail

--- src/Component/Layout/Header/index.jsx
+++ src/Component/Layout/Header/index.jsx
@@ -18,74 +18,76 @@ function Header() {
   const [isSideActive, setIsSideActive] = useState(false);
   return (
     <header className="header">
-      <div className="header_primary">
-        <ActionButton
-          className="header_hamburger"
-          handleClick={() => setIsSideActive(true)}
-        >
-          <Menu />
-        </ActionButton>
-        <img src={NavLogo} alt="nav_logo" className="header_img" />
-        <Navbar />
-        {createPortal(
-          <Sidebar isActive={isSideActive} handleClose={setIsSideActive} />,
-          document.body
-        )}
-      </div>
-      <div className="nav_btns">
-        <ModalWrap actionIcon={<Login />}>
-          <div className="sign_up_form_wrap">
-            <div className="form_head">
-              <img src={NavLogo} alt="nav_logo" />
-              <h2>Sign Up</h2>
+      <div className="header_wrap">
+        <div className="header_primary">
+          <ActionButton
+            className="header_hamburger"
+            handleClick={() => setIsSideActive(true)}
+          >
+            <Menu />
+          </ActionButton>
+          <img src={NavLogo} alt="nav_logo" className="header_img" />
+          <Navbar />
+          {createPortal(
+            <Sidebar isActive={isSideActive} handleClose={setIsSideActive} />,
+            document.body
+          )}
+        </div>
+        <div className="nav_btns">
+          <ModalWrap actionIcon={<Login />}>
+            <div className="sign_up_form_wrap">
+              <div className="form_head">
+                <img src={NavLogo} alt="nav_logo" />
+                <h2>Sign Up</h2>
+              </div>
+              <form className="sign_up_form">
+                <input name="firstName" placeholder="First Name" type="text" />
+                <input name="lastName" placeholder="Last Name" type="text" />
+                <input
+                  name="displayName"
+                  placeholder="Display Name"
+                  type="text"
+                />
+                <input name="countryName" placeholder="Country" type="text" />
+                <input name="email" placeholder="Email" type="email" />
+                <input name="password" placeholder="Password" type="password" />
+                <ActionButton className="sign_up_btn">Sign Up</ActionButton>
+              </form>
+              <div className="sign_up_meta">
+                <p className="is_sign_in">
+                  Have an Account? <Link>Sign in</Link>
+                </p>
+                <ActionLink>Back to Store</ActionLink>
+              </div>
             </div>
-            <form className="sign_up_form">
-              <input name="firstName" placeholder="First Name" type="text" />
-              <input name="lastName" placeholder="Last Name" type="text" />
-              <input
-                name="displayName"
-                placeholder="Display Name"
-                type="text"
-              />
-              <input name="countryName" placeholder="Country" type="text" />
-              <input name="email" placeholder="Email" type="email" />
-              <input name="password" placeholder="Password" type="password" />
-              <ActionButton className="sign_up_btn">Sign Up</ActionButton>
-            </form>
-            <div className="sign_up_meta">
-              <p className="is_sign_in">
-                Have an Account? <Link>Sign in</Link>
-              </p>
-              <ActionLink>Back to Store</ActionLink>
+          </ModalWrap>
+          <ModalWrap actionIcon={<Login />}>
+            <div className="sign_in_form_wrap">
+              <div className="form_head">
+                <img src={NavLogo} alt="nav_logo" />
+                <h2>Sign In</h2>
+              </div>
+              <form className="sign_in_form">
+                <input name="email" type="email" placeholder="Email" />
+                <input name="password" type="password" placeholder="Password" />
+                <label className="sign_in_remember">
+                  <input type="checkbox" />
+                  <span>Remember Me</span>
+                </label>
+                <ActionButton>Sign In</ActionButton>
+              </form>
+              <div className="sign_up_meta">
+                <p className="is_forget">
+                  Forgot your password? <Link>Reset</Link>
+                </p>
+                <p className="is_sign_up">
+                  Don't have an Account? <Link>Sign Up</Link>
+                </p>
+                <ActionLink>Back to Store</ActionLink>
+              </div>
             </div>
-          </div>
-        </ModalWrap>
-        <ModalWrap actionIcon={<Login />}>
-          <div className="sign_in_form_wrap">
-            <div className="form_head">
-              <img src={NavLogo} alt="nav_logo" />
-              <h2>Sign In</h2>
-            </div>
-            <form className="sign_in_form">
-              <input name="email" type="email" placeholder="Email" />
-              <input name="password" type="password" placeholder="Password" />
-              <label className="sign_in_remember">
-                <input type="checkbox" />
-                <span>Remember Me</span>
-              </label>
-              <ActionButton>Sign In</ActionButton>
-            </form>
-            <div className="sign_up_meta">
-              <p className="is_forget">
-                Forgot your password? <Link>Reset</Link>
-              </p>
-              <p className="is_sign_up">
-                Don't have an Account? <Link>Sign Up</Link>
-              </p>
-              <ActionLink>Back to Store</ActionLink>
-            </div>
-          </div>
-        </ModalWrap>
+          </ModalWrap>
+        </div>
       </div>
     </header>
   );
