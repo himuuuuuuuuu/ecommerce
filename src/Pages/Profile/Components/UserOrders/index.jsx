5a6ca5249a4891beb610a9db13002a8f2004a87f
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-06-05 01:47:10 -0700

    STYLE: Empty Order List

--- src/Pages/Profile/Components/UserOrders/index.jsx
+++ src/Pages/Profile/Components/UserOrders/index.jsx
@@ -2,15 +2,25 @@ import React from "react";
 
 import "./UserOrders.css";
 import { useData } from "../../../../Context/DataContext";
-import { OrderCard } from "../../../../Component";
+import { ActionLink, OrderCard } from "../../../../Component";
+import EmptyOrder from "../../../../Asset/Order/EmptyOrder.svg";
 
 function UserOrders() {
   const { state } = useData();
 
   return (
     <div className="orders">
-      <h3 className="orders_head">ORDER SUMMARY</h3>
-      {state.orderList.length === 0 && <h3>No Order Placed</h3>}
+      {state.orderList.length === 0 && (
+        <div className="orders_empty">
+          <p className="orders_empty_text">
+            Looks like you haven't placed an order
+          </p>
+          <img src={EmptyOrder} alt="empty_order" />
+          <ActionLink className="orders_btn" reach="/productList">
+            Explore
+          </ActionLink>
+        </div>
+      )}
       {state.orderList.length > 0 &&
         state.orderList.map((current) => {
           return <OrderCard {...current} key={current.id} />;
